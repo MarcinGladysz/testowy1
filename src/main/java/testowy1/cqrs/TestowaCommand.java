11424f9a45e8e06b07760608a909657e0afb8ccb
MarcinG <mgladysz1502@gmail.com>
2018-07-09 23:13:58 +0200

    json fix

--- src/main/java/testowy1/cqrs/TestowaCommand.java
+++ src/main/java/testowy1/cqrs/TestowaCommand.java
@@ -6,9 +6,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @CommandHandlerAssigment(handlerClass = TestowyHandler.class)
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        property = "type")
 public class TestowaCommand extends Command {
 
     public TestowaCommand(String name1) {
--- src/main/java/testowy1/cqrs/UpdateCommand.java
+++ src/main/java/testowy1/cqrs/UpdateCommand.java
@@ -5,9 +5,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @CommandHandlerAssigment(handlerClass = UpdateCommandHandler.class)
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        property = "type")
 public class UpdateCommand extends Command {
 
     public UpdateCommand(String name1) {
